eef349f5c4f5b0f09e2210afd492b4939cf43ed3
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-05-19 15:19:08 +0300

    not work

---
+++ server/teams.py
@@ -0,0 +1,18 @@
+teams = [
+    {"id": 1, "name": "Lenny", "score": 10},
+    {"id": 2, "name": "Alonso", "score": 23},
+    {"id": 3, "name": "Al", "score": 51},
+    {"id": 4, "name": "Dewayne", "score": 132},
+    {"id": 5, "name": "Ahmed", "score": 42},
+    {"id": 6, "name": "Lena", "score": 13},
+    {"id": 7, "name": "Theodore", "score": 21},
+    {"id": 8, "name": "Christa", "score": 61},
+    {"id": 9, "name": "Teresa", "score": 123},
+    {"id": 10, "name": "Wilburn", "score": 42},
+    {"id": 11, "name": "Juana", "score": 76},
+    {"id": 12, "name": "Abby", "score": 53},
+    {"id": 13, "name": "Carla", "score": 12},
+    {"id": 14, "name": "Francisca", "score":11},
+    {"id": 15, "name": "Margaret", "score": 52}
+]
+
